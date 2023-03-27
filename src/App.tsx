14327cd397752b7041e52e26eb6398b336b7dfcb
juliusBaranauskas <22618356+juliusBaranauskas@users.noreply.github.com>
2023-03-27 23:05:30 +0300

    add memeTV implementation

---
+++ src/App.tsx
@@ -0,0 +1,92 @@
+import { Component, createEffect, createSignal } from 'solid-js';
+import useRedditApi, { Post } from './hooks/useRedditApi';
+import styles from './App.module.css';
+
+const memeSubreddits = [
+  'memes',
+  'aww',
+  'darkstockphotos',
+  'ProgrammerHumor',
+  'okbuddyretard',
+]
+
+const App: Component = () => {
+  const { get, isReady } = useRedditApi({
+    username: '',
+    password: '',
+    appId: '',
+    appSecret: '',
+  });
+
+  const [posts, setPosts] = createSignal<Post[]>([]);
+  const [previouslyShown, setPreviouslyShown] = createSignal<string[]>([]);
+  const [alreadyFetched, setAlreadyFetched] = createSignal(false);
+
+  const firstMeme = () => posts().length === 0 ? undefined : posts()[0];
+
+  const updatePostList = (retrievedPosts: Post[]) => {
+    // filter already fetched
+    let newPosts = retrievedPosts.filter(post => posts().find(oldPost => oldPost.id === post.id) === undefined);
+    
+    // filter memes already shown
+    newPosts = newPosts.filter(post => !previouslyShown().includes(post.id));
+    setPosts(prev => prev.concat(newPosts));
+  };
+
+  const fetchSomeMemes = (count: number = 5) => {
+    if (!isReady()) return;
+
+    const randomizedSubreddit = memeSubreddits[Math.floor(Math.random()*(memeSubreddits.length - 1))];
+
+    get(`/r/${randomizedSubreddit}/hot`, { // rising
+      g: 'GLOBAL',
+      timeframe: 'day',
+      limit: `${count}`,
+    }).then(updatePostList);
+  };
+
+  createEffect(() => {
+    if (!isReady() || alreadyFetched()) return;
+
+    fetchSomeMemes();
+    setAlreadyFetched(true);
+  });
+
+  createEffect(() => console.log(posts()));
+
+  const tabVisibilityHandler = () => {
+    if (document.hidden) {
+      console.log('tab deactivated, archiving meme that was shown');
+      if (!firstMeme()) return;
+
+      setPreviouslyShown(prev => prev.concat(firstMeme()!.id))
+
+      // remove post from list of memes to be shown
+      setPosts(prev => prev.slice(1));
+    } else {
+        console.log('tab activated');
+        fetchSomeMemes();
+    }
+  };
+
+  createEffect(() => {
+    document.addEventListener('visibilitychange', tabVisibilityHandler);
+    return () => document.removeEventListener('visibilitychange', tabVisibilityHandler);
+  });
+
+
+  const currentPost = () => {
+    if (!firstMeme()) return <p>Loading your memes...</p>
+
+    return <img src={firstMeme()!.url} class={styles.memeImage} alt="meme" />;
+  };
+
+  return (
+    <div class={styles.App}>
+      <div class={styles.postHeader}></div>
+        {currentPost()}
+    </div>
+  );
+};
+
+export default App;
---
+++ src/hooks/useRedditApi.ts
@@ -0,0 +1,134 @@
+import { createEffect, createSignal } from "solid-js";
+import { Buffer } from 'buffer';
+import { encode } from "querystring";
+
+export type Post = {
+  id: string;
+  authorName: string;
+  createdAtUTC: number;
+  subreddit: string;
+  score: number;
+  title: string;
+  ups: number;
+  downs:number;
+  url: string;
+}
+
+type RedditCredentials = {
+  username: string,
+  password: string,
+  appId: string,
+  appSecret: string,
+}
+
+type RedditApi = {
+  get: (url: string, data: Record<string, any>) => Promise<any>;
+  isReady: () => boolean;
+};
+
+const useRedditApi = (redditCredentials: RedditCredentials): RedditApi => {
+  const { appId, appSecret, password, username } = redditCredentials;
+  const [token, setToken] = createSignal<string | undefined>(undefined);
+  const [tokenExpireDate, setTokenExpireDate] = createSignal<string | undefined>(undefined);
+  const [latestAfter, setLatestAfter] = createSignal<string | undefined>(undefined);
+
+  const authHeader = () => `Basic ${Buffer.from(`${appId}:${appSecret}`).toString('base64')}`;
+
+  createEffect(() => {
+    const body = new FormData();
+    body.append('grant_type', 'password');
+    body.append('username', username);
+    body.append('password', password);
+
+    fetch('https://www.reddit.com/api/v1/access_token', {
+      method: 'POST',
+      headers: {
+        'Authorization': authHeader(),
+      },
+      body,
+    }).then(async response => {
+      if (response.status !== 200) {
+        console.error('AAAAA it failed to get token');
+        return;
+      }
+
+      const {
+        access_token: accessToken,
+        expires_in: expiresIn,
+        token_type: tokenType
+      } = await response.json()
+
+      setToken(`${tokenType} ${accessToken}`);
+      // Shorten token expiration time by half to avoid race condition where
+      // token is valid at request time, but server will reject it
+      setTokenExpireDate(((Date.now() / 1000) + expiresIn) / 2);
+    });
+  });
+
+  const get = async (url: string, data: Record<string, string>) => {
+
+    if (!token()) {
+      console.log('token not yet valid, skipping get request');
+      return;
+    }
+
+    const requestData = {
+      ...data,
+      after: latestAfter() ?? '',
+    };
+
+    const baseUrl = 'https://oauth.reddit.com';
+    const queryString = encode(requestData);
+    const urlWithQueryString = `${baseUrl}${url}?${queryString}`;
+
+    const response = await fetch(urlWithQueryString, {
+      method: 'GET',
+      headers: {
+        'Authorization': token()!,
+      },
+    });
+
+    if (response.status !== 200) {
+      console.error('AAAAA it failed to get reddit stuff');
+      return;
+    }
+
+    const json = await response.json();
+    setLatestAfter(json.data.after);
+
+    const responseJson = json.data.children as Array<any>;
+
+    // filter by post type
+    const onlyImages = post => 
+      post.kind === 't3' &&
+      post.data.post_hint === 'image' &&
+      post.data.is_video === false;
+
+    const linkPosts = responseJson.filter(onlyImages);
+
+    const posts: Post = linkPosts.map(post => {
+      const postData = post.data;
+
+      return {
+        id: `${postData.subreddit}_${postData.id}`,
+        authorName: postData.author,
+        createdAtUTC: postData.created_utc,
+        downs: postData.downs,
+        subreddit: postData.subreddit,
+        score: postData.score,
+        title: postData.title,
+        ups: postData.ups,
+        url: postData.url,
+      };
+    })
+
+    return posts;
+  };
+
+  return {
+    get,
+    isReady: () => token() !== undefined,
+  }
+};
+
+export default useRedditApi;
